4e882029851767a3557c9ebf2d3db8976d0ce857
yootd <yootdh@naver.com>
2023-07-26 16:33:40 +0900

    Day05

--- src/main/java/com/mega/it/springboot/web/IndexController.java
+++ src/main/java/com/mega/it/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.mega.it.springboot.web;
 
+import com.mega.it.springboot.config.auth.LoginUser;
 import com.mega.it.springboot.config.auth.dto.SessionUser;
 import com.mega.it.springboot.service.posts.PostsService;
 import com.mega.it.springboot.web.dto.PostsListResponseDto;
@@ -21,9 +22,9 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping(value = "/")
-        public String index(Model model) {
+        public String index(Model model, @LoginUser SessionUser user) {
             model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName1", user.getName());
         }
